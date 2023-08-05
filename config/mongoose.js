c50b8b0ec2dd688347f455350e143922551f494b
Ankush Pine <ankushpineofficial@gmail.com>
2023-08-05 20:31:35 +0530

    Updated

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const DB = process.env.MONGO_DB_URL;
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = mongoose;
